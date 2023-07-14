6ca06b3cbbfbea86c2091e45e5713196e2003b8a
Taichi-Sawahata <taichi.sawahata@gmail.com>
2023-07-14 10:04:47 +0900

    second commit

--- src/main/java/com/example/demo/calculator/CalculatorService.java
+++ src/main/java/com/example/demo/calculator/CalculatorService.java
@@ -1,27 +1,25 @@
 package com.example.demo.calculator;
 
-
 import org.springframework.stereotype.Service;
-
 
 @Service
 public class CalculatorService {
-	
+
 	public int calculate(int num1, int num2, String option) {
-		
+
 		switch (option) {
-		
+
 		case "+":
-			return  num1 + num2;
-		
+			return num1 + num2;
+
 		case "-":
-			return	 num1 - num2;
-			
+			return num1 - num2;
+
 		case "*":
-		 	return  num1 *  num2;
-		
+			return num1 * num2;
+
 		default:
-			return  num1 /  num2;
+			return num1 / num2;
 		}
 	}
 }
